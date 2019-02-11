bf88ada763829055312112e66f98acedad621e67
zshi2 <zshi2@student.unimelb.edu.au>
2019-02-11 11:53:25 +1100

    spinner removed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,29 +5,8 @@ import {MDBView, MDBContainer, MDBCol, MDBRow, Animation, MDBIcon} from 'mdbreac
 import Milan_Card from "../../Component/Card/milan_card";
 import Cattolica_Footer from "../../Component/CattolicaFooter/Cattolica_Footer";
 import {Helmet} from "react-helmet";
-import { css } from '@emotion/core';
-// First way to import
-import { CircleLoader } from 'react-spinners';
-
-const override = css`
-    display: block;
-    margin: 0 auto;
-    border-color: red;
-`;
-
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true
-        }
-    }
-
-    componentDidMount() {
-        this.setState({loading: 'false'});
-    }
-
 
     render () {
 
@@ -38,18 +17,7 @@ class Home extends React.Component {
                     <title>Study Abroad at UNIVERSITÀ CATTOLICA</title>
                 </Helmet>
                 <Animation type="fadeIn">
-                    <div>
-                        <CircleLoader
-                            css={override}
-                            sizeUnit={"px"}
-                            size={150}
-                            color={'#123abc'}
-                            loading={this.state.loading}
-                        />
-                        <MDBView className="banner" />
-                    </div>
-
-
+                    <MDBView className="banner" />
                     <br/>
                     <MDBContainer>
                         <MDBRow>
